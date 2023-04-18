58904f888dc68b812a5426877330f71e99e10065
Akshay-9189 <akshaykumbhar957@gmail.com>
2023-04-18 19:44:23 +0530

    removed caching mechanism

--- OrderService/src/main/java/com/csi/service/OrderService.java
+++ OrderService/src/main/java/com/csi/service/OrderService.java
@@ -2,14 +2,12 @@ package com.csi.service;
 
 import com.csi.dto.response.CreateOrderDto;
 import com.csi.model.OrderDetails;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 public interface OrderService {
 
-    @Cacheable(value = "orderId")
     CreateOrderDto getOrder(String orderId);
 
     List<CreateOrderDto> saveOrder(String shoppingCartId);
