7b997fd301b7fd7d74cf8ae785ae26db2724ac20
Akshay-9189 <akshaykumbhar957@gmail.com>
2023-04-18 20:02:39 +0530

    added caching mechanism

--- ProductService/src/main/java/com/csi/dao/ProductDao.java
+++ ProductService/src/main/java/com/csi/dao/ProductDao.java
@@ -1,22 +1,28 @@
 package com.csi.dao;
 
-import com.csi.exception.ProductNotFound;
 import com.csi.model.Product;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductDao {
 
+    @Cacheable(value = "Products")
     List<Product> getAllProduct();
 
+    @CacheEvict(value = {"Products", "ProductById", "ProductByName"}, allEntries = true)
     Product saveProduct(Product product);
 
+    @Cacheable(value = "ProductById")
     Product getProduct(String productId);
 
+    @CacheEvict(value = {"Products", "ProductById", "ProductByName"}, allEntries = true)
     Product updateProduct(Product product);
 
+    @CacheEvict(value = {"Products", "ProductById", "ProductByName"}, allEntries = true)
     void deleteProduct(String productId);
 
+    @Cacheable(value = "ProductByName")
     Product getByProductName(String productName);
 }
