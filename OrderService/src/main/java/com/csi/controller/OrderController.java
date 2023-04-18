e6e7f64ab8fb7a7ed8323ce4c47e4b4a5b1b1554
Akshay-9189 <akshaykumbhar957@gmail.com>
2023-04-18 19:46:47 +0530

    added response entity type

--- OrderService/src/main/java/com/csi/controller/OrderController.java
+++ OrderService/src/main/java/com/csi/controller/OrderController.java
@@ -1,10 +1,14 @@
 package com.csi.controller;
 
+import com.csi.dto.response.CreateOrderDto;
+import com.csi.model.OrderDetails;
 import com.csi.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/order")
@@ -15,48 +19,48 @@ public class OrderController {
     private OrderService orderService;
 
     @GetMapping("/get-order/{orderId}")
-    public ResponseEntity<?> getOrderById(@PathVariable String orderId) {
+    public ResponseEntity<CreateOrderDto> getOrderById(@PathVariable String orderId) {
         return new ResponseEntity<>(orderService.getOrder(orderId), HttpStatus.OK);
     }
 
     @PostMapping("/create-order/{userId}")
-    public ResponseEntity<?> saveOrder(@PathVariable String userId) {
+    public ResponseEntity<List<CreateOrderDto>> saveOrder(@PathVariable String userId) {
         return new ResponseEntity<>(orderService.saveOrder(userId), HttpStatus.OK);
     }
 
     @GetMapping("/")
-    public ResponseEntity<?> getAllOrders() {
+    public ResponseEntity<List<OrderDetails>> getAllOrders() {
         return new ResponseEntity<>(orderService.orderList(), HttpStatus.OK);
     }
 
     @GetMapping("/reverse")
-    public ResponseEntity<?> getReverseList() {
+    public ResponseEntity<List<OrderDetails>> getReverseList() {
         return new ResponseEntity<>(orderService.reverseOrderList(), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete-order/{orderId}")
-    public ResponseEntity<?> deleteOrder(@PathVariable String orderId) {
+    public ResponseEntity<String> deleteOrder(@PathVariable String orderId) {
         orderService.deleteOrder(orderId);
         return new ResponseEntity<>("Order Deleted Successfully", HttpStatus.OK);
     }
 
     @GetMapping("/filter-by-product-name/{productName}")
-    public ResponseEntity<?> getFilterByProductName(@PathVariable String productName) {
+    public ResponseEntity<List<OrderDetails>> getFilterByProductName(@PathVariable String productName) {
         return new ResponseEntity<>(orderService.filterByProductName(productName), HttpStatus.OK);
     }
 
     @GetMapping("/filter-by-user-id/{userId}")
-    public ResponseEntity<?> getFilterBByUserId(@PathVariable String userId) {
+    public ResponseEntity<List<OrderDetails>> getFilterBByUserId(@PathVariable String userId) {
         return new ResponseEntity<>(orderService.filterByUserId(userId), HttpStatus.OK);
     }
 
     @GetMapping("/filter-by-current-date")
-    public ResponseEntity<?> filterByCurrentData() {
+    public ResponseEntity<List<OrderDetails>> filterByCurrentData() {
         return new ResponseEntity<>(orderService.filterByCurrentDate(), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete-order-by-productId/{productId}")
-    public ResponseEntity<?> deleteOrderByProductId(@PathVariable String productId) {
+    public ResponseEntity<String> deleteOrderByProductId(@PathVariable String productId) {
         orderService.deleteOrderByProductId(productId);
         return new ResponseEntity<>("Order Deleted Successfully", HttpStatus.OK);
     }
